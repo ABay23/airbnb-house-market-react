b12c36751e143f51d1d916435e9743bb60608fc1
Alejandro Bay <alejandrobay23@gmail.com>
2022-12-04 22:48:16 -0500

    Sign In and Sign up

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
+import bedicon from '../assets/svg/bedicon.svg'
+import bathtubIcon from '../assets/svg/bathtubIcon.svg'
+
+const ListingItem = () => {
+  return <div>ListingItem</div>
+}
+
+export default ListingItem
