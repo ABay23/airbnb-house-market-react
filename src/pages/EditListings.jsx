12cb117b96afaef9c41d274786ccacc5d9da4348
Alejandro Bay <alejandrobay23@gmail.com>
2022-12-07 07:01:33 -0500

    Edit and Delete

--- src/pages/EditListings.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const EditListings = () => {
-  return <div>EditListings</div>
-}
-
-export default EditListings
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -97,6 +97,10 @@ const Profile = () => {
     }
   }
 
+  const onEdit = (listingId) => {
+    navigate(`/edit-listing/${listingId}`)
+  }
+
   return (
     <div className='profile'>
       <header className='profileHeader'>
@@ -155,6 +159,7 @@ const Profile = () => {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
